c8e15204b71817db7c52701a72305bcebbfc2448
Sam Herwig <sam@consumeandcreate.co>
2026-01-23 11:13:19 -0700

    redirect rules

--- src/lib/imageUrl.js
+++ src/lib/imageUrl.js
@@ -1,4 +1,4 @@
-import createImageUrlBuilder from '@sanity/image-url'
+import { createImageUrlBuilder } from '@sanity/image-url'
 
 const projectId = import.meta.env.VITE_SANITY_PROJECT_ID
 const dataset = import.meta.env.VITE_SANITY_DATASET || 'production'
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'redirect-root',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/' || req.url === '') {
+            res.writeHead(302, { Location: '/map/' })
+            res.end()
+            return
+          }
+          next()
+        })
+      },
+    },
+  ],
   base: '/map/',
   assetsInclude: ['**/*.glsl'],
 })
